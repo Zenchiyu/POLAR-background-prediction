3e779f23492b28c8ea91a37e5516f5f09729a3c7
Stéphane Liem Nguyen <stephane13ste@gmail.com>
2023-07-13 17:53:30 +0200

    moved code for splitting data within load_data.py

--- src/load_data.py
+++ src/load_data.py
@@ -1,5 +1,7 @@
 import numpy as np
 import uproot as ur
+from sklearn.model_selection import train_test_split
+
 
 def load_TTree(root_filename="../data/Allaux_Bfield.root",
                        TTree_name="t_hk_obox",
@@ -37,3 +39,29 @@ def load_data_as_dict(root_filename="../data/Allaux_Bfield.root",
         data_dict |= data.arrays(features_name, library="np")
     return data_dict
     
+
+
+def train_val_test_split(X, y, val_size=0.2, test_size=0.2, random_state=42, shuffle=True):
+    """
+    Split the dataset into train, validation and test sets.
+    X and y are pandas dataframes
+    """
+    assert val_size + test_size < 1, "There's no training examples, need some training examples" 
+    X_train, X_test, y_train, y_test = train_test_split(X, y,
+                                                        test_size=test_size, 
+                                                        random_state=random_state, 
+                                                        shuffle=shuffle)
+    X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, 
+                                                      test_size=val_size/(1-test_size), 
+                                                      random_state=random_state,
+                                                      shuffle=shuffle) 
+    # 1/4 = 20/80 = 0.2/(0.2+0.6)
+    # Reset indices of df:
+    X_train.reset_index(drop=True, inplace=True)
+    X_val.reset_index(drop=True, inplace=True)
+    X_test.reset_index(drop=True, inplace=True)
+    y_train.reset_index(drop=True, inplace=True)
+    y_val.reset_index(drop=True, inplace=True)
+    y_test.reset_index(drop=True, inplace=True)
+    
+    return X_train, X_val, X_test, y_train, y_val, y_test
